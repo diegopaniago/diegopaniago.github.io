f71009677a76cecc724731e14613a46563112356
diegopaniago <diegopaniago1@gmail.com>
2019-02-20 01:59:39 -0300

    Setting up reactjs

---
+++ build/precache-manifest.10671d1c64e4929dbf3502259192a6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "13a79a62b3074b52e9b6",
+    "url": "/static/js/main.13a79a62.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "13a79a62b3074b52e9b6",
+    "url": "/static/css/main.e422c40d.chunk.css"
+  },
+  {
+    "revision": "20ffbb6d37f417debe8a5c0b17337395",
+    "url": "/index.html"
+  }
+];
